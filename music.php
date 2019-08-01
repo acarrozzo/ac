61c54225e5d731d2dab0d168344f0127df8a66c1
Anthony Carrozzo <acarrozzo@gmail.com>
2019-07-31 20:28:08 -0400

    AC SITE - monster party - july commit

--- music.php
+++ music.php
@@ -2,137 +2,6 @@
 
 
 <div id="center">
-
-
-
-	<!--
-		<div class="collapser XjustMobile">
-			<h2>Singles</h2>
-		</div>
-		<div class="collapserContent">
-
-		<section>
-			<figure class="left gal" data-pop>
-				<img src="img/music/art/IPlantATree_SongArt.jpg" width="800" height="800" alt="I plant a tree it grows slow song art">
-			</figure>
-			<h3>I Plant a Tree, It Grows Slow.</h3>
-			<h4>ss4st</h4>
-			<p>One of my longer instrumentals composed of 3 unique parts. A nice mix of keys, strings, synths, and percussion, I really enjoyed making this one.</p>
-			<audio src="img/music/mp3s/iPlantATree_ItGrowsSlow.mp3" controls>
-			Your browser does not support this audio format.
-			</audio>
-
-
-			<hr>
-
-			<figure class="left gal" data-pop>
-				<img src="img/music/art/oneclip_SongArt.jpg" width="800" height="800" alt="one clip song art">
-			</figure>
-			<h3>One Clip</h3>
-			<h4>ss4st</h4>
-			<p>I just used one clip, the percussion shaker. The rest is all me.</p>
-			<audio src="img/music/mp3s/oneclip.mp3" controls>
-			Your browser does not support this audio format.
-			</audio>
-
-
-			<hr>
-
-			<figure class="left gal" data-pop>
-				<img src="img/music/art/Caine'sPokerFace_SongArt.jpg" width="800" height="800" alt="caine's poker face song art">
-			</figure>
-			<h3>Caine's Poker Face</h3>
-			<h4>ss4st</h4>
-			<p>Quick sound clip made for a short film.</p>
-			<audio src="img/music/mp3s/CainesPokerFace.mp3" controls>
-			Your browser does not support this audio format.
-			</audio>
-
-
-			<hr>
-
-			<figure class="left gal" data-pop>
-				<img src="img/music/art/Einstein'sPipe_SongArt.jpg" width="800" height="800" alt="einstein's pipe song art">
-			</figure>
-			<h3>Einstein's Pipe</h3>
-			<h4>ss4st</h4>
-			<p>Some nice strings, percussion and the sort. </p>
-			<audio src="img/music/mp3s/EinsteinsPipe.mp3" controls>
-			Your browser does not support this audio format.
-			</audio>
-
-
-			<hr>
-
-			<figure class="left gal" data-pop>
-				<img src="img/music/art/KrakenVsDinosaur_SongArt.jpg" width="800" height="800" alt="kraven vs dinosaur song art">
-			</figure>
-			<h3>Kraken vs. Dinosaur</h3>
-			<h4>ss4st</h4>
-			<p>Beasts & Beats</p>
-			<audio src="img/music/mp3s/KrakenVsDinosaur.mp3" controls>
-			Your browser does not support this audio format.
-			</audio>
-
-
-			<hr>
-
-			<figure class="left gal" data-pop>
-				<img src="img/music/art/Proxima_SongArt.jpg" width="800" height="800" alt="proxima alpha centauri one song art">
-			</figure>
-			<h3>Proxima</h3>
-			<h4>ss4st</h4>
-			<p>Not one Sun</p>
-			<audio src="img/music/mp3s/Proxima.mp3" controls>
-			Your browser does not support this audio format.
-			</audio>
-
-
-			<hr>
-
-			<figure class="left gal" data-pop>
-				<img src="img/music/art/TooDark_SongArt.jpg" width="800" height="800" alt="too dark song art">
-			</figure>
-			<h3>Too Dark</h3>
-			<h4>ss4st</h4>
-			<p>I don't even like this song man. It's too dark.</p>
-			<audio src="img/music/mp3s/TooDark.mp3" controls>
-			Your browser does not support this audio format.
-			</audio>
-
-
-			<hr>
-
-			<figure class="left gal" data-pop>
-				<img src="img/music/art/weird_SongArt.jpg" width="800" height="800" alt="weird song art">
-			</figure>
-			<h3>Weird</h3>
-			<h4>ss4st</h4>
-			<p>Strings, choirs, bass, beats, acoustic guitars and some jinglies.</p>
-			<audio src="img/music/mp3s/weird.mp3" controls>
-			Your browser does not support this audio format.
-			</audio>
-
-
-			<hr>
-			<figure class="left gal" data-pop>
-				<img src="img/music/art/JeremiahRemix_SongArt.jpg" width="800" height="800" alt="jeremiah remix song art">
-			</figure>
-			<h3>Jeremiah (Remix)</h3>
-			<h4>ss4st</h4>
-			<p>Made this while at Steve and Barry's with some other folks in the design department. 100% Pure fun.</p>
-			<audio src="img/music/mp3s/Jeremiah-Remix.mp3" controls>
-			Your browser does not support this audio format.
-			</audio>
-
-
-		</section>
-
-
-	</div>
--->
-
-
 <div class="collapser expanded">
 	<h2>Strange Sounds for Strange Times</h2>
 </div>
@@ -141,7 +10,7 @@
 <p><strong>SS4ST</strong> is my solo music project. I like to create random tunes here and there for fun. I have finally compiled over 40 instrumentals into 3 albums and have them up on Spotify, iTunes, Amazon Music, and more. Enjoy.</p>
 <hr>
 <figure class="right50 gal" data-pop>
-<img src="img/music/art/Drift_AlbumArt.jpg" width="800" height="800" alt="Drift Album Art - Strange Sounds for Strange Times - Anthony Carrozzo music">
+<img src="img/music/art/drift_album_art.jpg" width="800" height="800" alt="Drift Album Art - Strange Sounds for Strange Times - Anthony Carrozzo music">
 </figure>
 <h3>Drift</h3>
 <h4>2018 | 7 songs</h4>
